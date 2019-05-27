a1f25a66859166d0cfd92997657dfc4d6eaf3311
haotianz <haotianz@stanford.edu>
2019-05-27 14:29:34 -0700

    upgrade to latest rekall vgrid

--- app/deps/install.sh
+++ app/deps/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-RUN_TESTS=${RUN_TESTS:=0}
+RUN_TESTS=${RUN_TESTS:=1}
+NPM_COMMAND="npm install --unsafe-perm"
+PIP_COMMAND="pip3 install --force-reinstall --user -e ."
 
 # Fail fast
 set -e
@@ -13,13 +15,13 @@ pushd .
 cd $DEPS_DIR
 echo "Installing Rekall"
 cd rekall/rekallpy
-pip3 install --upgrade --force-reinstall --user -e .
+$PIP_COMMAND
 if [ $RUN_TESTS == 1 ]; then
         python3 setup.py test
 fi
 cd $DEPS_DIR
 cd rekall/rekalljs
-npm install
+$NPM_COMMAND
 npm run build
 npm link
 
@@ -39,7 +41,7 @@ echo "Installing Caption-Index"
 cd caption-index
 rustup update
 rustup override set nightly
-pip3 install --upgrade --force-reinstall --user .
+$PIP_COMMAND
 ./get_models.sh
 if [ $RUN_TESTS == 1 ]; then
         python3 setup.py test
@@ -51,26 +53,28 @@ echo "Installing Rs-Embed"
 cd rs-embed
 rustup update
 rustup override set nightly
-pip3 install --upgrade --force-reinstall --user .
+$PIP_COMMAND
 if [ $RUN_TESTS == 1 ]; then
         python3 setup.py test
 fi
 
 cd $DEPS_DIR
 echo "Installing vgrid"
-cd vgrid
-npm install
+cd vgrid/vgridjs
+$NPM_COMMAND
 npm link @wcrichto/rekall
 npm run build
 npm link
+cd ../vgridpy
+$PIP_COMMAND
 
 cd $DEPS_DIR
 echo "Installing vgrid_jupyter"
 cd vgrid_jupyter
-npm install
+$NPM_COMMAND
 npm link @wcrichto/vgrid
 npm run build
-pip3 install --upgrade --force-reinstall --user -e .
+$PIP_COMMAND
 
 jupyter nbextension enable --py --user widgetsnbextension
 jupyter contrib nbextension install --user --skip-running-check
@@ -83,11 +87,6 @@ jupyter nbextension enable --py --user qgrid
 
 jupyter nbextension install vgrid_jupyter --py --symlink --user --overwrite
 jupyter nbextension enable vgrid_jupyter --py --user
-
-cd /app
-npm link vgrid
-npm install
-npm run build
 
 popd
 
